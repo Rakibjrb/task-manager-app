e431de75e431d627d14c9fa711a58332f2c01f04
rakibjrb <rakibjrb572157@gmail.com>
2023-12-21 00:38:24 +0600

    completed navbar

--- src/Layout/Main/Nav/Nav.jsx
+++ src/Layout/Main/Nav/Nav.jsx
@@ -1,8 +1,69 @@
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { FaBarsStaggered } from "react-icons/fa6";
+import { IoCloseSharp } from "react-icons/io5";
+import "./nav.css";
+
+const links = (
+  <>
+    <li>
+      <NavLink to={"/"}>Home</NavLink>
+    </li>
+    <li>
+      <NavLink to={"/features"}>Features</NavLink>
+    </li>
+    <li>
+      <NavLink to={"/benefits"}>Benefits</NavLink>
+    </li>
+    <li>
+      <NavLink to={"/about"}>About</NavLink>
+    </li>
+    <li className="lg:hidden bg-green-500 text-center py-2 rounded-md">
+      <NavLink to={"/login"}>Login</NavLink>
+    </li>
+  </>
+);
+
 const Nav = () => {
+  const [mobileNav, setMobileNav] = useState(false);
+
   return (
-    <div>
-      <nav>navbar</nav>
-    </div>
+    <nav className="max-w-7xl mx-auto py-5 flex justify-between px-4 xl:px-0">
+      <h3 className="text-xl uppercase font-bold">
+        Task <span className="text-green-600">Legend</span>
+      </h3>
+      <div className="flex gap-5 items-center">
+        <ul className="hidden gap-5 uppercase font-semibold lg:flex">
+          {links}
+        </ul>
+        <ul
+          className={`${
+            mobileNav ? "" : "hidden"
+          } lg:hidden uppercase absolute right-0 top-14 bg-base-300 py-10 px-8 w-[290px] flex flex-col gap-3 rounded-lg`}
+        >
+          {links}
+        </ul>
+        <NavLink to={"/login"}>
+          <h3 className="font-semibold uppercase hidden lg:block py-2 px-5 bg-green-500 rounded-md hover:bg-gray-400 transition-colors">
+            Login
+          </h3>
+        </NavLink>
+        <div>
+          <button
+            className="lg:hidden"
+            onClick={() => {
+              setMobileNav(!mobileNav);
+            }}
+          >
+            {mobileNav ? (
+              <IoCloseSharp className="text-xl" />
+            ) : (
+              <FaBarsStaggered className="text-xl" />
+            )}
+          </button>
+        </div>
+      </div>
+    </nav>
   );
 };
 
